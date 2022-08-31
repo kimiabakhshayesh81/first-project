d688225a15a32e5024534fed93df97032a82fef5
kimiabakhshayesh81 <kimia.bakhsha@gmial.com>
2022-08-31 13:27:35 +0430

    minro changes

--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Header = () => {
     return (
         <div className="App">
-            <h1>Where in the word?</h1>
+            <h1>Where in the word?!!</h1>
         </div>
         
 
